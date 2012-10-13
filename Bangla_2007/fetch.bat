8328cd2f93e9ec541a25e79da2e0e002158fc3ca
j-s-f <john.s.fisher@btinternet.com>
2012-10-13 12:02:29 +0100

    Robins wedding

---
+++ Bangla_2007/fetch.bat
@@ -0,0 +1,42 @@
+cp "f:/photo_archive/Bangladesh2007/PICT9790.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/GJG/DSCN3805.JPG" GJG/.
+cp "f:/photo_archive/Bangladesh2007/GJG/DSCN3807.JPG" GJG/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05129.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/RJG/PICT9823.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 012.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 015.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 019.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 025.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/GJG/DSCN3843.JPG" GJG/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0073.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0115.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0158.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0184.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0281.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0240.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0446.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 056.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/AHN/IMGP0383.JPG" AHN/.
+cp "f:/photo_archive/Bangladesh2007/GJG/DSCN3895.JPG" GJG/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0582.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/RJG/PICT9893.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05234.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/RJG/PICT9943.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/RJG/PICT9910.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 062.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0430.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0460.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05220.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0541.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0544.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05278.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0566.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/RJG/PICT9813.JPG" RJG/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05305.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05196.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05335.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0743.JPG" SKF/.
+cp "f:/photo_archive/Bangladesh2007/SKF/day4+5 114.jpg" SKF/.
+cp "f:/photo_archive/Bangladesh2007/JRL/DSC05381.JPG" JRL/.
+cp "f:/photo_archive/Bangladesh2007/SKF/IMG_0821.JPG" SKF/.
+
